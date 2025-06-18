1f3f375c7788dfd3533c16b360e59ed736c2a5de
Drake <dariocostarelli88@gmail.com>
2025-06-18 12:38:13 +0100

    JS: Add player movement and treasure collection logic in update function

--- js/game.js
+++ js/game.js
@@ -1,14 +1,20 @@
-<<<<<<< HEAD
+// Global variables
+
 let background;
 let player;
 let dragon;
 let treasure;
 
 // Create a new scene
-=======
->>>>>>> 5a84bd069f913caea69061891a0bc5d366e040df
+
 let gameScene = new Phaser.Scene('Game');
 
+// Initiate
+gameScene.init = function () {
+  this.playerSpeed = 3;
+};
+
+// Preload
 gameScene.preload = function () {
   this.load.image('background', 'assets/background.png');
   this.load.image('player', 'assets/player.png');
@@ -16,22 +22,52 @@ gameScene.preload = function () {
   this.load.image('treasure', 'assets/treasure.png');
 };
 
-<<<<<<< HEAD
+// Create
+
 gameScene.create = function () {
-  background = this.add.image(0, 0, 'background').setOrigin(0, 0);
+  background = this.add.image(0, 0, 'background');
+  background.setOrigin(0, 0);
+
   player = this.add.sprite(70, 180, 'player');
+
   dragon = this.add.sprite(250, 180, 'dragon');
+
   treasure = this.add.sprite(this.sys.game.config.width - 80, this.sys.game.config.height / 2, 'treasure');
   treasure.setScale(0.6);
 };
 
-=======
->>>>>>> 5a84bd069f913caea69061891a0bc5d366e040df
+// Update
+
+gameScene.update = function () {
+  // Move the player
+  if (this.input.activePointer.isDown) {
+    player.x += this.playerSpeed;
+  }
+
+  // treasure collection
+  let playerRect = player.getBounds();
+  let treasureRect = treasure.getBounds();
+
+  if (Phaser.Geom.Intersects.RectangleToRectangle(playerRect, treasureRect)) {
+    this.scene.manager.bootScene(this);
+  }
+};
+
+function grow(item) {
+  if (item.scaleX < 2 && item.scaleY < 2) {
+    item.setScale(item.scaleX + 0.001, item.scaleY + 0.001);
+  }
+}
+
+// Set the configuration of the game
+
 let config = {
   type: Phaser.AUTO,
   width: 640,
   height: 360,
   scene: gameScene,
 };
+
+// Create a new game and pass the configuration
 
 let game = new Phaser.Game(config);
